346807953202559b942ab1db5d726c44160739b6
Vatsal Gupta <gvatsal60@gmail.com>
2024-05-12 10:42:47 +0530

    Added ShellCheck and actions

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
+# shellcheck shell=sh
+
 # Function to check if .aliases.sh is sourced in specified rc file and append it if not
 check_sourcing() {
     chmod +x "$HOME/.aliases.sh"
 
     # Check if .aliases.sh is sourced in specified rc file, if not append it
-    if ! grep -qxF 'source "${HOME}/.aliases.sh"' "$HOME/$1"; then
+    if ! grep -qxF "source \"${HOME}/.aliases.sh\"" "$HOME/$1"; then
         printf "\n# Sourcing custom aliases" >>"${HOME}/$1"
         echo "source \"\${HOME}/.aliases.sh\"" >>"${HOME}/$1"
     fi
 
     if [ -r "${HOME}/$1" ]; then
+        # shellcheck source=/dev/null
         if ! . "${HOME}/$1"; then
             echo "Error: Unable to source ${HOME}/$1"
         else
@@ -33,19 +36,19 @@ check_rc_files() {
 }
 
 # Function to download alias file using wget
-function dw_alias_file_wget() {
+dw_alias_file_wget() {
     wget -O "$HOME/.aliases.sh" "https://raw.githubusercontent.com/gvatsal60/Linux-Aliases/master/.aliases.sh"
     check_rc_files # Call function to check rc files after downloading alias file
 }
 
 # Function to download alias file using curl
-function dw_alias_file_curl() {
+dw_alias_file_curl() {
     curl -o "$HOME/.aliases.sh" "https://raw.githubusercontent.com/gvatsal60/Linux-Aliases/master/.aliases.sh"
     check_rc_files # Call function to check rc files after downloading alias file
 }
 
 # Function to download alias file using git
-function dw_alias_file_git() {
+dw_alias_file_git() {
     git clone --depth=1 "https://github.com/gvatsal60/Linux-Aliases.git" "$HOME/.aliases"
     cp "$HOME/.aliases/.aliases.sh" "$HOME/.aliases.sh"
     rm -rf "$HOME/.aliases"
@@ -57,13 +60,13 @@ if [ -f "$HOME/.aliases.sh" ]; then
     echo "File already exists: $HOME/.aliases.sh"
 else
     # Check if wget is available
-    if command -v wget &>/dev/null; then
+    if command -v wget >/dev/null 2>&1; then
         dw_alias_file_wget # Call function to download alias file using wget
     # Check if curl is available
-    elif command -v curl &>/dev/null; then
+    elif command -v curl >/dev/null 2>&1; then
         dw_alias_file_curl # Call function to download alias file using curl
     # Check if git is available
-    elif command -v git &>/dev/null; then
+    elif command -v git >/dev/null 2>&1; then
         dw_alias_file_git # Call function to download alias file using git
     else
         echo "Either install wget, curl, or git"
