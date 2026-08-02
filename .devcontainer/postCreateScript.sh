fee1bda8c042c7d7825c828144a70e4a82d904c7
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2026-08-02 11:20:17 +0530

    Remove # Date: comments from all files

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -3,7 +3,6 @@
 ###################################################################################################
 # File: postCreateScript.sh
 # Author: [Vatsal Gupta (gvatsal60)]
-# Date: 07-Aug-2024
 # Description:
 # This script runs automatically after the DevContainer environment has been created.
 # It performs various initialization tasks to ensure the development environment is properly configured.
