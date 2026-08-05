1d2220c6c1667ed8980859e1678ed3eaa71d7593
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2026-08-05 19:55:56 +0530

    chore: add set -o pipefail to postCreateScript.sh (#128)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -35,6 +35,13 @@ readonly ALIAS_SRC_URL="https://raw.githubusercontent.com/gvatsal60/Linux-Aliase
 ###################################################################################################
 # Functions
 ###################################################################################################
+curl_https() {
+    if ! command -v curl >/dev/null 2>&1; then
+        echo "Error: curl is not installed. Aborting." >&2
+        return 1
+    fi
+    curl -fsSL --proto '=https' "$@"
+}
 
 ###################################################################################################
 # Main Script
@@ -42,12 +49,7 @@ readonly ALIAS_SRC_URL="https://raw.githubusercontent.com/gvatsal60/Linux-Aliase
 
 # Install Linux aliases from external script using curl and execute immediately
 # Note: Make sure to review scripts fetched from external sources for security reasons
-if command -v curl >/dev/null 2>&1; then
-    curl -fsSL "${ALIAS_SRC_URL}" | sh
-else
-    echo "Error: curl is not installed. Unable to use Linux aliases"
-    exit 1
-fi
+curl_https "${ALIAS_SRC_URL}" | sh
 
 # As bind mounts not supported in GitHub Codespaces
 if [ -n "${CODESPACE_NAME}" ]; then
